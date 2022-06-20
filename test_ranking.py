6fffa00b985200bf642c3f62bb6b404e53509fa6
u1and0 <e01.ando60@gmail.com>
2022-06-20 17:34:55 +0900

    [mod] set environment arg for unittest

--- test_ranking.py
+++ test_ranking.py
@@ -1,6 +1,6 @@
 """ranking.py test"""
+import os
 import unittest
-import ranking
 
 
 class TestRanks_shift(unittest.TestCase):
@@ -42,11 +42,11 @@ class TestRanks_order(unittest.TestCase):
         actual = ranks.order(top, ids)
         expected = [
             "1. :arrow_upper_right: [/rank3](https://demo.growi.org/3333) \
-:heart:2 :footprints:0 :left_speech_bubble:0 :pencil2:0",
+:heart:2 :footprints:0 :speech_balloon:0 :pencil2:0",
             "2. :arrow_right: [/rank2](https://demo.growi.org/2222) \
-:heart:1 :footprints:0 :left_speech_bubble:0 :pencil2:0",
+:heart:1 :footprints:0 :speech_balloon:0 :pencil2:0",
             "3. :arrow_lower_right: [/rank1](https://demo.growi.org/1111) \
-:heart:0 :footprints:0 :left_speech_bubble:0 :pencil2:0",
+:heart:0 :footprints:0 :speech_balloon:0 :pencil2:0",
         ]
         self.assertEqual(actual, expected)
 
@@ -65,14 +65,18 @@ class TestRanks_order_newcase(unittest.TestCase):
         actual = ranks.order(top, ids)
         expected = [
             "1. :new: [/rank4](https://demo.growi.org/4444) \
-:heart:10 :footprints:0 :left_speech_bubble:0 :pencil2:0",
+:heart:10 :footprints:0 :speech_balloon:0 :pencil2:0",
             "2. :arrow_upper_right: [/rank3](https://demo.growi.org/3333) \
-:heart:2 :footprints:0 :left_speech_bubble:0 :pencil2:0",
+:heart:2 :footprints:0 :speech_balloon:0 :pencil2:0",
             "3. :arrow_lower_right: [/rank2](https://demo.growi.org/2222) \
-:heart:1 :footprints:0 :left_speech_bubble:0 :pencil2:0",
+:heart:1 :footprints:0 :speech_balloon:0 :pencil2:0",
         ]
         self.assertEqual(actual, expected)
 
 
 if __name__ == "__main__":
+    # Set env for test
+    os.environ["GROWI_ACCESS_TOKEN"] = ""
+    os.environ["GROWI_URL"] = "https://demo.growi.org"
+    import ranking
     unittest.main()
